e6e6502a52cd575363762c8b2c9e6225adc85f4f
frenks <shabani1@web.de>
2024-05-06 14:48:28 +0200

    Add leere Ordner: Model, ControllerAPI, Repo, Service

---
+++ backend/src/main/java/com/github/frenk93/backend/Controller/ControllerAPI.java
@@ -0,0 +1,12 @@
+package com.github.frenk93.backend.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+@RequiredArgsConstructor
+public class ControllerAPI {
+
+
+}
