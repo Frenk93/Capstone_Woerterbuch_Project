5314607c62eb7da14d7c5c96f15b83b4aadc7e12
frenks <shabani1@web.de>
2024-05-24 16:18:04 +0200

    add Constructor for ChatGPTRequest

--- backend/src/main/java/com/github/frenk93/backend/Model/ChatGPTRequest.java
+++ backend/src/main/java/com/github/frenk93/backend/Model/ChatGPTRequest.java
@@ -1,9 +1,14 @@
 package com.github.frenk93.backend.Model;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public record ChatGPTRequest(
         String model,
         List<ChatGPTMessage> messages
 ) {
+    ChatGPTRequest (String message){
+        this("gpt-3.5-turbo", Collections.singletonList(new ChatGPTMessage("user", message)));
+    }
 }
