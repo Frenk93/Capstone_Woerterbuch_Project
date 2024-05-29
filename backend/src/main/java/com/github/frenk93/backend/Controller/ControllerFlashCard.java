0e9d2ce4b60ce98977c24c0690d9a98f4b76a266
frenks <shabani1@web.de>
2024-05-29 14:06:35 +0200

    feature Flashcards: add Get, Post and Delete Endpoints

--- backend/src/main/java/com/github/frenk93/backend/Controller/ControllerFlashCard.java
+++ backend/src/main/java/com/github/frenk93/backend/Controller/ControllerFlashCard.java
@@ -1,9 +1,33 @@
 package com.github.frenk93.backend.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.frenk93.backend.Model.Entry;
+import com.github.frenk93.backend.Service.ServiceAPI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/flashcards")
+@RequiredArgsConstructor
 public class ControllerFlashCard {
+    private final ServiceAPI serviceFlash;
+
+    @GetMapping()
+    public List<Entry> getAllEntries(){
+        return serviceFlash.findAllEntries();
+    }
+
+    @PostMapping()
+    public void createEntry(@RequestBody Entry entry) {
+        serviceFlash.setEntry(entry);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEntry(@PathVariable String id) {
+        serviceFlash.deleteEntry(id);
+    }
+
+
+
 }
