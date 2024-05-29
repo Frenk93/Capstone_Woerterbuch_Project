c06e4774f71f0c0cfb9a90bfc01cb51e19b2e8e8
frenks <shabani1@web.de>
2024-05-29 13:49:26 +0200

    feature Flashcards: add new ControllerFlashCard

---
+++ backend/src/main/java/com/github/frenk93/backend/Controller/ControllerFlashCard.java
@@ -0,0 +1,9 @@
+package com.github.frenk93.backend.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/flashcards")
+public class ControllerFlashCard {
+}
